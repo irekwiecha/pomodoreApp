3086c88424d465bba71cbd5f0b5485eb2e270404
irekwiecha <irek88@gmail.com>
2023-09-22 21:35:50 +0200

    feat: complete user interface

--- main.py
+++ main.py
@@ -21,11 +21,24 @@ window = tk.Tk()
 window.title("Pomodoro App")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = tk.Label(text="Timer", font=(FONT_NAME, 50))
+title_label.config(fg=GREEN, bg=YELLOW)
+title_label.grid(column=1, row=0)
+
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = tk.Button(text="Start", activebackground=PINK)
+start_button.grid(column=0, row=2)
+reset_button = tk.Button(text="Reset", activebackground=PINK)
+reset_button.grid(column=2, row=2)
+
+check_label = tk.Label(text="✓", font=(FONT_NAME, 12, "bold"))
+check_label.config(fg=GREEN, bg=YELLOW)
+check_label.grid(column=1, row=3)
 
 
 window.mainloop()
