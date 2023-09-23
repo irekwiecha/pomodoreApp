d1067bc2ebf641bde45fb7fe4bc93ee3ddd43ebd
irekwiecha <irek88@gmail.com>
2023-09-23 12:26:46 +0200

    feat: complete the timer and countdown mechanism

--- main.py
+++ main.py
@@ -6,16 +6,39 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 20
+reps = 0
+timer = ""
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_label.config(text="")
+    global reps
+    reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_sec = SHORT_BREAK_MIN * 60
+    long_sec = LONG_BREAK_MIN * 60
+    if reps % 8 == 0:
+        title_label.config(text="Break", fg=RED)
+        count_down(long_sec)
+    elif reps % 2 != 0:
+        title_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+    elif reps % 2 == 0:
+        title_label.config(text="Break", fg=PINK)
+        count_down(short_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -24,7 +47,15 @@ def count_down(count):
     count_sec = count % 60
     canvas.itemconfig(timer_text, text=f"{count_min:02d}:{count_sec:02d}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_session = reps // 2
+        for _ in range(work_session):
+            marks += "✓"
+        check_label.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -44,10 +75,10 @@ canvas.grid(column=1, row=1)
 
 start_button = tk.Button(text="Start", command=start_timer, activebackground=PINK)
 start_button.grid(column=0, row=2)
-reset_button = tk.Button(text="Reset", activebackground=PINK)
+reset_button = tk.Button(text="Reset", command=reset_timer, activebackground=PINK)
 reset_button.grid(column=2, row=2)
 
-check_label = tk.Label(text="✓", font=(FONT_NAME, 12, "bold"))
+check_label = tk.Label(font=(FONT_NAME, 12, "bold"))
 check_label.config(fg=GREEN, bg=YELLOW)
 check_label.grid(column=1, row=3)
 
